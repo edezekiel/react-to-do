9bb75e61d18741793505af4b7ae3f359eb0cb473
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2019-06-09 11:33:57 -0500

    add todo using hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-const Todo = ({ todo }) => <div className="todo">{todo.text}</div>
+const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
 
 function TodoForm({ addTodo }) {
   const [value, setValue] = useState("");
@@ -11,37 +11,39 @@ function TodoForm({ addTodo }) {
     if (!value) return;
     addTodo(value);
     setValue("");
-  }
+  };
 
   return (
     <form onSubmit={handleSubmit}>
       <input
-        type='text'
+        type="text"
         className="input"
         value={value}
         onChange={e => setValue(e.target.value)}
-    />
+      />
     </form>
-  )
+  );
 }
 
 function App() {
   const [todos, setTodos] = useState([
-    { text: "Learn about React"},
-    { text: "Meet friend for lunch"},
-    { text: "Build really cool todo app"}
+    { text: "Learn about React", isCompleted: false },
+    { text: "Meet friend for lunch", isCompleted: false },
+    { text: "Build really cool todo app", isCompleted: false }
   ]);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
 
   return (
     <div className="App">
       <div className="todo=list">
         {todos.map((todo, index) => (
-          <Todo
-            key={index}
-            index={index}
-            todo={todo}
-            />
+          <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   );
